d4a05fc278cce20921c1076d6b28dc11677a91e2
harry <95003118+HarryPillyze@users.noreply.github.com>
2025-03-27 14:58:20 +0900

    add insertStepData

--- lib/pedometer_db.dart
+++ lib/pedometer_db.dart
@@ -9,9 +9,11 @@ import 'dart:io';
 
 import 'package:pedometer/pedometer.dart';
 import 'package:pedometer_db/provider/step_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 import 'package:flutter/services.dart';
 
+import 'model/step.dart';
 import 'pedometer_db_method_channel.dart';
 
 class PedometerDb {
@@ -34,5 +36,22 @@ class PedometerDb {
 
   Future<int> insertPedometerData(StepCount event) async {
     return await _stepProvider.insertData(event) ?? 0;
+  }
+
+  Future<void> insertStepData({
+    required DateTime timeStamp
+  }) async {
+
+    Step? step = await _stepProvider.getLastStep();
+    await _stepProvider.db?.insert(
+      tableName, // table name
+      {
+        'total': (step?.total ?? 0)+1,
+        'last': (step?.last ?? 0)+1,
+        'timestamp': timeStamp.millisecondsSinceEpoch,
+        'plus': 1,
+      }, // new post row data
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
   }
 }
